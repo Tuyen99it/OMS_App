938b857b062a883889c01a7278773bcbef9f60e5
Tuyen99it <nguyentuyen99tnut@gmail.com>
2025-02-13 23:19:47 +0700

    Custom Login 2fa Action

--- Areas/IdentityMVC/Controllers/AccountController.cs
+++ Areas/IdentityMVC/Controllers/AccountController.cs
@@ -73,7 +73,7 @@ namespace OMS_Webapp.Areas.Identity.Controllers
                     var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
                     code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
                     var callbackUrl = Url.Action(
-                        "/Account/ConfirmEmail",
+                        "/ConfirmEmail",
                         controller: "Account",
                         values: new
                         {
@@ -88,7 +88,7 @@ namespace OMS_Webapp.Areas.Identity.Controllers
                         $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
                     if (_userManager.Options.SignIn.RequireConfirmedAccount)
                     {
-                        return RedirectToPage("RegisterConfirmation", new { email = model.Email, returnUrl = returnUrl });
+                        return RedirectToAction("RegisterConfirmation", new { email = model.Email, returnUrl = returnUrl });
 
                     }
                     else
@@ -107,118 +107,33 @@ namespace OMS_Webapp.Areas.Identity.Controllers
             return View();
         }
 
-        //Get: /Account/ConfirmEmail
-        [HttpGet("/confirmemail/")]
-        public async Task<IActionResult> ConfirmEmail(string userId, string code)
-        {
-            if (userId == null || code == null) return RedirectToAction("/Index");
-            var user=await _userManager.FindByIdAsync(userId);
-            if (user == null) return NotFound();
-            code=Encoding.UTF8.GetString(WebEncoders.Base64UrlDecode(code));
-            var result=await _userManager.ConfirmEmailAsync(user, code);
-            ViewBag.StatusMessage=result.Succeeded? "Thank you for confirming your email." : "Error confirming your email.";
-            return View();
-        }
-
-        //Get:/Account/ForgetPassword
-        [HttpGet("/forgotpassword/")]
+        //Get: /Account/RegisterConfirmation: Allow confirm register when using fake email addres
+        [HttpGet("/registerconfirmation/")]
         [AllowAnonymous]
-        public async Task<IActionResult>ForgotPassword(string returnUrl = null)
+        public async Task<IActionResult> RegisterConfirmation( string email,string returnUrl=null,bool displayConfirmationAccountLink=false)
         {
-            returnUrl ??= Url.Content("~/");
-            return View();
-        }
-        //Get: //Account/ConfirmEmailChange
-        [HttpGet("/confirmemailchange/")]
-        [AllowAnonymous]
-        public async Task<IActionResult> ConfirmEmailChange(string userId, string email, string code)
-        {
-            if (userId == null || email == null || code == null)
+           if(email == null)
             {
                 return RedirectToAction("/Index");
             }
-
-            var user = await _userManager.FindByIdAsync(userId);
-            if (user == null)
+            returnUrl ??= Url.Content("~/");
+            var user=await _userManager.FindByEmailAsync(email);
+            if (user == null) return NotFound("Can not find out user have email: " + email);
+            // Create a code confirmation when use fake email. When you use real email please remove this code or pass value "false" to displayConfirmationAccountLink
+            if (displayConfirmationAccountLink)
             {
-                return NotFound($"Unable to load user with ID '{userId}'.");
-            }
-
-            code = Encoding.UTF8.GetString(WebEncoders.Base64UrlDecode(code));
-            var result = await _userManager.ChangeEmailAsync(user, email, code);
-            if (!result.Succeeded)
-            {
-                ViewBag.StatusMessage = "Error changing email.";
-                return View();
-            }
-
-            // In our UI email and user name are one and the same, so when we update the email
-            // we need to update the user name.
-            var setUserNameResult = await _userManager.SetUserNameAsync(user, email);
-            if (!setUserNameResult.Succeeded)
-            {
-                ViewBag.StatusMessage = "Error changing user name.";
-                return View();
-            }
-
-            await _signInManager.RefreshSignInAsync(user);
-            ViewBag.StatusMessage = "Thank you for confirming your email change.";
-            return View();
-        }
-
-        //Get: /Accout/ExternalLogin
-        [HttpGet("/externallogin/")]
-        public IActionResult ExternalLogin () => RedirectToPage("./Login");
-
-
-        //Post: /Account/forgotpassword/
-        [HttpPost("/forgotpassword/")]
-        [AllowAnonymous]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> ForgotPassword(ForgotPasswordViewModel model)
-        {
-            if (ModelState.IsValid)
-            {
-                var user = await _userManager.FindByEmailAsync(model.Email);
-                if (user == null || !(await _userManager.IsEmailConfirmedAsync(user)))
-                {
-                    // Don't reveal that the user does not exist or is not confirmed
-                    return RedirectToAction("./ForgotPasswordConfirmation");
-                }
-                var code=await _userManager.GeneratePasswordResetTokenAsync(user);
-                _logger.LogInformation("Code:{code}",code);
+                var userId = await _userManager.GetUserIdAsync(user);
+                var code=await _userManager.GenerateEmailConfirmationTokenAsync(user);
                 code=WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
-                _logger.LogInformation("Encode:{code}", code);
-                var callbackUrl = Url.Action(
-                    "ResetPassword",
+                ViewBag.EmailConfirmationUrl = Url.Action(
+                    "ConfirmEmail",
                     controller: "Account",
-                    values: new { area = "Identity", code },
+                    values: new { area = "Identity", userId = userId, code = code, returnUrl = returnUrl },
                     protocol: Request.Scheme);
-                await _emailSender.SendEmailAsync(model.Email, "ResetPassword", $"Please reset your password by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}' clicking here</a>");
-                return RedirectToAction("./ForgotPasswordConfirmation");
-
             }
-            return View();
+            return View();  
         }
 
-        //Get:/Account/forgotpasswordconfirmation
-        [HttpGet("/forgotpasswordconfirmation/")]
-        [AllowAnonymous]
-        public IActionResult ForgotPasswordConfirmation()
-        {
-            return View();
-        }
-
-
-        //Get:/Account/AccessDenied
-        [HttpGet("/accessdenied/")]
-        [AllowAnonymous]
-        public IActionResult AccessDenied()
-        {
-            return View();
-        }
-
-        //
         //Get: /Acount/Login/url
         [HttpGet("/login/")]
         [AllowAnonymous]
@@ -270,22 +185,252 @@ namespace OMS_Webapp.Areas.Identity.Controllers
                 if (result.IsLockedOut)
                 {
                     _logger.LogWarning(2, "Account is locked");
-                    return View("Lockout");
+                    return RedirectToAction("Lockout");
                 }
                 else
                 {
                     ModelState.AddModelError(string.Empty, "Invalid login attempt");
                     return View(model);
                 }
-               
+
             }
             return View(model);
 
 
         }
 
-       
+        //Get: /Account/LoginWith2fa
+        [HttpGet("/loginwith2fa/")]
+        [AllowAnonymous]
+        public async Task<IActionResult> LoginWith2Fa(bool rememerMe,string returnUrl=null)
+        {
+            // Ensure the user has gon through the username and password screem first
+            var user = await _signInManager.GetTwoFactorAuthenticationUserAsync();
+            if (user == null)
+            {
+                throw new InvalidOperationException("Unable to load two-factor authentication user");
+            }
+            ViewBag.ReturnUrl = returnUrl;  
+            ViewBag.RememberMe = rememerMe;
+            return View();
+        }
 
+        //Post: / Account/LoginWith2Fa
+        [HttpPost("/loginwith2fa/")]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> LoginWith2Fa(LoginWith2faViewModel model,bool rememerMe,string returnUrl=null )
+        {
+            if (!ModelState.IsValid) return View();
+            returnUrl ??= Url.Content("~/");
+            var user = await _signInManager.GetTwoFactorAuthenticationUserAsync();
+            if (user == null)
+            {
+                throw new InvalidOperationException($"Unable to load two-factor authentication user.");
+            }
+            var authenticatorCode = model.TwoFactorCode.Replace(" ", string.Empty).Replace("-", string.Empty);
+            var result = await _signInManager.TwoFactorAuthenticatorSignInAsync(authenticatorCode, rememerMe, model.RememberMachine);
+            var userId = await _userManager.GetUserIdAsync(user);
+            if (result.Succeeded)
+            {
+                _logger.LogInformation("User with ID '{UserId}' logged in with 2fa.", user.Id);
+                return LocalRedirect(returnUrl);
+            }
+            else if (result.IsLockedOut)
+            {
+                _logger.LogWarning("User with ID '{UserId}' account locked out.", user.Id);
+                return RedirectToAction("Lockout");
+            }
+            else
+            {
+                _logger.LogWarning("Invalid authenticator code entered for user with ID '{UserId}'.", user.Id);
+                ModelState.AddModelError(string.Empty, "Invalid authenticator code.");
+                return View();
+            }
+            
+        }
+
+        //Get: /Account/LoginWithRecoveryCode/
+        [HttpGet("/loginwithrecoverycode/")]
+        public IActionResult LoginWithRecoveryCode()
+        {
+            return View();
+        }
+
+        //Post: /Account/loginwithrecoverycode
+        [HttpPost("/loginwithrecoverycode/")]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> LoginWithRecoveryCode(string returnUrl=null)
+        {
+            return View();
+        }
+
+        //Get: /Account/ExternalLogin
+        [HttpGet("/externallogin/")]
+        public async Task<IActionResult> ExternalLogin()
+        {
+            return View();
+        }
+
+
+        //Post: /Account/ExternalLogin/callbackUrl
+        [HttpPost("/externallogin/")]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> ExternalLogin(string callbackUrl)
+        {
+            return View();
+        }
+
+        //Get: /Account/Logout
+        [HttpGet("/logout/")]
+        public async Task<IActionResult> Logout()
+        {
+            return View();
+        }
+
+
+        //Post: /Account/Logout
+        [HttpPost("/logout/")]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Logout(string returnUrl)
+        {
+            return View();
+        }
+
+        //Get: /Account/ConfirmEmail
+        [HttpGet("/confirmemail/")]
+        public async Task<IActionResult> ConfirmEmail(string userId, string code)
+        {
+            if (userId == null || code == null) return RedirectToAction("/Index");
+            var user = await _userManager.FindByIdAsync(userId);
+            if (user == null) return NotFound();
+            code = Encoding.UTF8.GetString(WebEncoders.Base64UrlDecode(code));
+            var result = await _userManager.ConfirmEmailAsync(user, code);
+            ViewBag.StatusMessage = result.Succeeded ? "Thank you for confirming your email." : "Error confirming your email.";
+            return View();
+        }
+
+        //Get: //Account/ConfirmEmailChange
+        [HttpGet("/confirmemailchange/")]
+        [AllowAnonymous]
+        public async Task<IActionResult> ConfirmEmailChange(string userId, string email, string code)
+        {
+            if (userId == null || email == null || code == null)
+            {
+                return RedirectToAction("/Index");
+            }
+
+            var user = await _userManager.FindByIdAsync(userId);
+            if (user == null)
+            {
+                return NotFound($"Unable to load user with ID '{userId}'.");
+            }
+
+            code = Encoding.UTF8.GetString(WebEncoders.Base64UrlDecode(code));
+            var result = await _userManager.ChangeEmailAsync(user, email, code);
+            if (!result.Succeeded)
+            {
+                ViewBag.StatusMessage = "Error changing email.";
+                return View();
+            }
+
+            // In our UI email and user name are one and the same, so when we update the email
+            // we need to update the user name.
+            var setUserNameResult = await _userManager.SetUserNameAsync(user, email);
+            if (!setUserNameResult.Succeeded)
+            {
+                ViewBag.StatusMessage = "Error changing user name.";
+                return View();
+            }
+
+            await _signInManager.RefreshSignInAsync(user);
+            ViewBag.StatusMessage = "Thank you for confirming your email change.";
+            return View();
+        }
+
+        //Get: /Account/ResendEmailConfirmation
+        [HttpGet("/ResendEmailConfirmation/")]
+        public async Task<IActionResult> ResendEmailConfirmation()
+        {
+            return View();
+        }
+
+
+        //Get:/Account/ForgetPassword
+        [HttpGet("/forgotpassword/")]
+        [AllowAnonymous]
+        public async Task<IActionResult> ForgotPassword(string returnUrl = null)
+        {
+            returnUrl ??= Url.Content("~/");
+            return View();
+        }
+
+
+        //Post: /Account/forgotpassword/
+        [HttpPost("/forgotpassword/")]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> ForgotPassword(ForgotPasswordViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await _userManager.FindByEmailAsync(model.Email);
+                if (user == null || !(await _userManager.IsEmailConfirmedAsync(user)))
+                {
+                    // Don't reveal that the user does not exist or is not confirmed
+                    return RedirectToAction("./ForgotPasswordConfirmation");
+                }
+                var code = await _userManager.GeneratePasswordResetTokenAsync(user);
+                _logger.LogInformation("Code:{code}", code);
+                code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
+                _logger.LogInformation("Encode:{code}", code);
+                var callbackUrl = Url.Action(
+                    "ResetPassword",
+                    controller: "Account",
+                    values: new { area = "Identity", code },
+                    protocol: Request.Scheme);
+                await _emailSender.SendEmailAsync(model.Email, "ResetPassword", $"Please reset your password by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}' clicking here</a>");
+                return RedirectToAction("./ForgotPasswordConfirmation");
+
+            }
+            return View();
+        }
+
+
+        //Get:/Account/forgotpasswordconfirmation
+        [HttpGet("/forgotpasswordconfirmation/")]
+        [AllowAnonymous]
+        public IActionResult ForgotPasswordConfirmation()
+        {
+            return View();
+        }
+
+        //Get:/Account/Resetpassword
+        [HttpGet("/resetpassword/")]
+        [AllowAnonymous]
+        public IActionResult ResetPassword()
+        {
+            return View();
+        }
+
+        //Get:/Account/ResetpasswordConfirmation
+        [HttpGet("/resetpasswordconfirmation/")]
+        [AllowAnonymous]
+        public IActionResult ResetPasswordConfirmation()
+        {
+            return View();
+        }
+
+        //Get:/Account/Lockout
+        [HttpGet("/lockout/")]
+        [AllowAnonymous]
+        public IActionResult Lockout()
+        {
+            return View();
+        }
 
         // Create AppUser
         private AppUser CreateUser()
@@ -309,5 +454,8 @@ namespace OMS_Webapp.Areas.Identity.Controllers
             }
             return (IUserEmailStore<AppUser>)_userStore;
         }
+
+
+      
     }
 }
---
+++ Areas/IdentityMVC/Models/Account/LoginWith2faViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OMS_Webapp.Areas.Identity.Models
+{ 
+    public class LoginWith2faViewModel {
+        [Required]
+        [StringLength(7,ErrorMessage ="The {0} must be at lease {2} and at max {1} characters long",MinimumLength =6)]
+        [DataType(DataType.Text)]
+        [Display(Name ="Authentication code")]
+        public string TwoFactorCode { get; set; }
+
+        [Display(Name ="Remember this machine")]
+        public bool RememberMachine {  get; set; }
+    
+    
+    }
+
+
+
+
+}
