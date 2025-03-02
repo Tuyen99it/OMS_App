8a1909b31f8f85f1d58aa55155fb7119c61610f6
Tuyen99it <nguyentuyen99tnut@gmail.com>
2025-03-02 15:59:11 +0700

    Finish Authenticator

--- obj/Debug/net8.0/OMS_Webapp.AssemblyInfo.cs
+++ obj/Debug/net8.0/OMS_Webapp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OMS_Webapp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d81ab4ed4a9ecfa3a01ba12b735f904c0a47945")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a9f32a2ba831d16010f71136ba066437913200d4")]
 [assembly: System.Reflection.AssemblyProductAttribute("OMS_Webapp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OMS_Webapp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
