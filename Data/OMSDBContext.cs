bd5ea06b11d04d86b5e2e868d0d83ac0cc8438f9
Tuyen99it <nguyentuyen99tnut@gmail.com>
2025-03-30 23:03:43 +0700

    add post model

--- Data/OMSDBContext.cs
+++ Data/OMSDBContext.cs
@@ -3,6 +3,7 @@ using Microsoft.EntityFrameworkCore;
 using OMS_App.Models;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.AspNetCore.Identity;
+using OMS_App.Areas.Post.Models;
 namespace OMS_App.Data
 {
     public class OMSDBContext : IdentityDbContext<AppUser>
@@ -10,6 +11,8 @@ namespace OMS_App.Data
 
         public DbSet<AppUser> AppUsers { get; set; }
         public DbSet<Category> Categories { get; set; }
+        public DbSet<Post> Posts {get;set;}
+        public DbSet<PostCategory> PostCategories {get;set;}
         public OMSDBContext(DbContextOptions<OMSDBContext> options) : base(options) { }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
@@ -30,6 +33,8 @@ namespace OMS_App.Data
             {
                 entity.HasIndex(p => p.Slug);
             });
+            // Tạo mối quan hệ many -many between Post and Category bằng việc tạo key cho bảng bằng việc kết hợp PostId và CategoryId
+            modelBuilder.Entity<PostCategory>().HasKey(p=>new {p.PostId, p.CategoryId});
         }
     }
 }
